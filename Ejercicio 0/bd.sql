fdcd3095b8d46ba91a4d66f5a2b856f905746ba7
ivansanguezax <ivansanguezax@gmail.com>
2023-04-15 17:28:29 -0400

    ejercicio 0

--- Ejercicio 0/bd.sql
+++ Ejercicio 0/bd.sql
@@ -3,27 +3,31 @@ CREATE DATABASE MIBASEIVAN;
 USE MIBASEIVAN;
 
 CREATE TABLE INSCRIPCION (
+    id_Inscripcion int NOT NULL,
     CI_estudiante int  NOT NULL,
     sigla varchar(10)  NOT NULL,
-    nota_1 int  NOT NULL,
-    nota_2 int  NOT NULL,
-    nota_3 int  NOT NULL,
-    nota_final int  NOT NULL,
-    CONSTRAINT INSCRIPCION_IVAN_pk PRIMARY KEY (CI_estudiante)
+    nota1 int  NOT NULL,
+    nota2 int  NOT NULL,
+    nota3 int  NOT NULL,
+    notaFinal int  NOT NULL,
+    CONSTRAINT INSCRIPCION_pk PRIMARY KEY (id_Inscripcion)
 );
 
-CREATE TABLE PERSONA(
+CREATE TABLE PERSONA (
     CI int  NOT NULL,
-    nombre_completo varchar(45)  NOT NULL,
-    fecha_nacimiento date  NOT NULL,
+    nombre_Completo varchar(45)  NOT NULL,
+    fecha_De_Nacimiento date  NOT NULL,
     telefono varchar(45)  NOT NULL,
     departamento varchar(2)  NOT NULL,
-    CONSTRAINT PERSONA_IVAN_pk PRIMARY KEY (CI)
+    CONSTRAINT PERSONA_pk PRIMARY KEY (CI)
 );
 
 CREATE TABLE USUARIO (
     CI int  NOT NULL,
     usuario varchar(45)  NOT NULL,
     password varchar(45)  NOT NULL,
-    CONSTRAINT USUARIO_IVAN_pk PRIMARY KEY (CI)
+    CONSTRAINT USUARIO_pk PRIMARY KEY (CI)
 );
+
+ALTER TABLE USUARIO 
+ADD COLUMN rol VARCHAR(45) NOT NULL
